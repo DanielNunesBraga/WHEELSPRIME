1d31863f672723d4542d6dea0f134537b32fe68d
kjohn47 <joaomorgado0@gmail.com>
2015-11-25 21:42:37 +0000

    DAL

--- WheelsPrime/App_Start/IdentityConfig.cs
+++ WheelsPrime/App_Start/IdentityConfig.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
 using WheelsPrime.Models;
+using WheelsPrime.DAL;
 
 namespace WheelsPrime
 {
@@ -42,7 +43,7 @@ namespace WheelsPrime
 
         public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context) 
         {
-            var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context.Get<ApplicationDbContext>()));
+            var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context.Get<WheelsprimeDBContext>()));
             // Configure validation logic for usernames
             manager.UserValidator = new UserValidator<ApplicationUser>(manager)
             {
---
+++ WheelsPrime/DAL/WheelsprimeDBContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WheelsPrime.Models;
+
+namespace WheelsPrime.DAL
+{
+    public class WheelsprimeDBContext : IdentityDbContext<ApplicationUser>
+    {
+        public WheelsprimeDBContext()
+            : base("wheelsprimedb", throwIfV1Schema: false)
+        {
+        }
+
+        public static WheelsprimeDBContext Create()
+        {
+            return new WheelsprimeDBContext();
+        }
+    }
+}
